f7123d7590c94ece689e0f397319975f8acb60d4
Leonardo <lfm.fiap@gmail.com>
2024-10-24 11:43:52 -0300

    Estilizando o projeto.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,24 @@
+// CRIANDO A FUNCTION VALIDAR
+function validar(){
+
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    // IF & ELSE PARA VALIDAÇÃO
+    if(usuario == "admin" && senha == "12345"){
+        alert(`Seja bem vindo, ${usuario}!!!`)
+        window.open("dashboard.html")
+    }
+    else{
+        alert("Usuário/Senha inválidos.")
+    }
+
+}
+
 function inserir(){
+
     let numero = document.getElementById('posicao').value -1;
     let novo = document.getElementById('novo').value;
     document.getElementsByClassName('time')[numero].innerHTML = novo;
-}
 
+}
